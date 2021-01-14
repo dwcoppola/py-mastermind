c7befbf8220eadd8033f2784c457e206fa262667
David <66121421+dwcoppola@users.noreply.github.com>
2021-01-14 05:38:34 -0500

    Add files via upload

---
+++ mastermind.py
@@ -0,0 +1,36 @@
+# 8, 10, or 12 guesses
+
+from random import random
+from random import shuffle
+
+def code_generator(colors, blanks, dupes):
+    rand = 6
+    color_list = ["red", "yellow", "green", "blue", "black", "white"]
+    shuffle(color_list)
+    if colors >= 7:
+        rand += 1
+        color_list.append("orange")
+        shuffle(color_list)
+        if colors == 8:
+            rand += 1
+            color_list.append("purple")
+            shuffle(color_list)
+    if blanks == True:
+        rand += 1
+        color_list.append("blank")
+        shuffle(color_list)
+    new_list = []
+    if dupes == True:
+        while len(new_list) < 4:
+            new_list.append(color_list[int(random() * rand)])
+    else: 
+        while len(new_list) < 4:
+            color = color_list[int(random() * rand)]
+            if color in new_list:
+                continue
+            else:
+                new_list.append(color)
+    return new_list
+
+def analyze_guess(code, guess):
+    pass
